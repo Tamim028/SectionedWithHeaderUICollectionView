2f39f2cae672738839eb90ca64fccdcee8aa4312
Tamim Dari Chowdhury <dari.tamim028@gmail.com>
2024-09-30 23:19:24 +0600

    [update] Section right inset updated.

--- SectionedContent/Source Files/ViewControllers/SectionedContentVC/SectionedContentVC.swift
+++ SectionedContent/Source Files/ViewControllers/SectionedContentVC/SectionedContentVC.swift
@@ -231,7 +231,7 @@ extension SectionedContentCollectionView: UICollectionViewDataSource, UICollecti
         if section == (self.cellViewModels.count - 1){
             return UIEdgeInsets(top: 0, left: 2, bottom: 0, right: 0) //no right inset for last
         }else{
-            return UIEdgeInsets(top: 0, left: 2, bottom: 0, right: 0)
+            return UIEdgeInsets(top: 0, left: 2, bottom: 0, right: 20)
         }
     }
     
